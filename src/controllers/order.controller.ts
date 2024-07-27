4b0103767a80a0dcb05f040b70c1a7d033409b77
Yash Vijay Bombale <yashbombale@gmail.com>
2024-07-27 19:53:51 +0530

    complete cash order route

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,70 +1,38 @@
 import { Request, Response } from "express";
 import { Store } from "../models/store.model";
-import User from "../models/user.model";
 import Order from "../models/store-orders.model";
+import Conversation from "../models/conversation.model";
 
 export const createOrder = async (req: Request, res: Response) => {
-  const { imageUrl, prescription } = req.body;
-  const { storeId, userId } = req.query;
-
-  if (!storeId || !userId) {
-    return res
-      .status(400)
-      .json({ message: "Both storeId and userId is required" });
+  const { conversationId, paymentMode } = req.query;
+  if (!conversationId || !paymentMode) {
+    return res.status(400).json("All query params is required");
   }
-
-  if (!imageUrl || !prescription) {
-    return res.status(400).json({
-      message: "All fields are required",
-    });
-  }
-
   try {
-    //check for storeId is valid or not;
-    const store = await Store.findById(storeId);
-    if (!store) {
-      return res.status(404).json({ message: "Invalid storeId" });
-    }
-    //if storeId is valid check userId is valid or not;
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({
-        message: "Invalid userId",
-      });
-    }
+    if (paymentMode === "cash") {
+      const updatedConversation = await Conversation.findByIdAndUpdate(
+        conversationId,
+        {
+          $set: {
+            isOrdered: true,
+            paymentMode: "cash",
+          },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
 
-    //checking if user has city,state,address,mobileNumber or not;
-    if (!user.city || !user.state || !user.address || !user.mobileNumber) {
-      return res.status(403).json({
-        message: "Shipping address is missing kindly update your profile",
-      });
+      if (!updatedConversation) {
+        return res.status(404).json("conversation not found");
+      }
+
+      return res.status(200).json(updatedConversation);
     }
-
-    //if both storeId and userId is valid;
-    // create Order;
-    const order = await Order.create({
-      storeId,
-      userId,
-      storeName: store.storeName,
-      orderedBy: user.username,
-      customerMobileNumber: user.mobileNumber,
-      userProfile: user.profilePicture,
-      prescriptionImage: imageUrl,
-      prescription,
-      deliveryCity: user.city,
-      deliveryState: user.state,
-      deliveryAddress: user.address,
-    });
-
-    return res.status(201).json({
-      message: "Order created",
-      order,
-    });
   } catch (error) {
-    console.log(`ERROR:IN CREATE-ORDER-CONTROLLER, ${error}`);
-    return res.status(500).json({
-      message: "ERROR:IN CREATE-STORE",
-    });
+    console.log(`ERROR:IN CREATE-ORDER-CONTROLLER,${error}`);
+    return res.status(500).json("ERROR:IN CREATE-ORDER-CONTROLLER");
   }
 };
 
